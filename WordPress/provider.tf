668a5c63dcf4d60d24a535f981ff1dbe160d3387
nagababudba0999 <67545689+nagababudba0999@users.noreply.github.com>
2020-06-29 11:02:16 +0530

    Update provider.tf

--- WordPress/provider.tf
+++ WordPress/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5WDV3YYGSECPGNGU"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
