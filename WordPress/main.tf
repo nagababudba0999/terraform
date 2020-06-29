757a06c870518ab13bb648ae7a2c93f04336caad
nagababudba0999 <67545689+nagababudba0999@users.noreply.github.com>
2020-06-29 11:01:48 +0530

    Update main.tf

--- WordPress/main.tf
+++ WordPress/main.tf
@@ -3,8 +3,8 @@ terraform {
 		bucket = "terraform-worpress-poc"
 		key    = "terraform/tfstate_1.tfstate"
 		region = "us-east-1"
-    access_key = "AKIA5WDV3YYGSECPGNGU"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 	}
 }
 
