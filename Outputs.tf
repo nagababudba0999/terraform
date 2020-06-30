c240705f1f6e53ff58e15e5914b8ad5571eb87bf
nagababudba0999 <67545689+nagababudba0999@users.noreply.github.com>
2020-06-30 22:25:36 +0530

    Update Outputs.tf

--- Outputs.tf
+++ Outputs.tf
@@ -2,10 +2,3 @@ output "instance-id" {
   value = module.ec2module.instance-id
 }
 
-output "ALB_DNSName" {
-  value = module.albmodule.ALB_DNSName
-}
-
-output "RDS_EndPoint" {
-  value = module.rdsmodule.RDS_Endpoint
-}
