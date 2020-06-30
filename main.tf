e4c3719710430e58cd0f26128411566b316a8fa6
nagababudba0999 <67545689+nagababudba0999@users.noreply.github.com>
2020-06-30 22:26:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,9 @@
-terraform {
-	backend "s3" {
-		bucket = "terraform-worpress-poc"
-		key    = "terraform/tfstate_1.tfstate"
-		region = "us-east-1"
-    access_key = "AKIA5WDV3YYGSECPGNGU"
-    secret_key = "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"
-	}
-}
 
+provider "aws" {
+  region     = "us-east-1"
+}
 
 module "ec2module"{
   source = "./Modules/ec2/"
 }
 
-module "albmodule"{
-  source = "./Modules/elb/"
-  instance-id = module.ec2module.instance-id
-}
-
-module "rdsmodule"{
-  source = "./Modules/rds"
-}
